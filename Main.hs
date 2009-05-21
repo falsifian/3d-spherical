24eca7875d5456ec283b7608ffc0836402ee3dc9
james <james@angel.falsifian.org>
2009-05-21 11:59:11 -0700

    Separated projection matrix setting; fixed sphere routine.

--- Main.hs
+++ Main.hs
@@ -18,12 +18,17 @@ main = do args_after_glut <- getArgs >>= initialize "Some Program" -- TODO: prog
           fix (\f -> modifyIORef state_ref update >> postRedisplay Nothing >> addTimerCallback 50 f)
           mainLoop
 
+set_projection_matrix :: IO ()
+set_projection_matrix =
+    -- The camera is sitting at the z pole looking toward the w pole.
+    do matrixMode $= Projection
+       loadIdentity
+       scale (0.5 :: GLfloat) 0.5 1e-5
+       matrixMode $= Modelview 0
+
 display :: IORef State -> IO ()
 display state_ref = do clear [ColorBuffer, DepthBuffer]
-                       matrixMode $= Projection
-                       loadIdentity
-                       scale (0.5 :: GLfloat) 0.5 1e-5
-                       matrixMode $= Modelview 0
+                       set_projection_matrix
                        materialAmbient FrontAndBack $= Color4 1 1 1 1
                        sphere 0.1
                        swapBuffers
@@ -32,7 +37,7 @@ sphere :: GLdouble -> IO ()
 sphere radius = -- radius is the radius in radians
 		-- A Euclidean sphere about the origin is a spherical sphere
                 -- about the w pole, so we just let GLU do this for us.
-                let euclid_radius = sin radius in
+                let euclid_radius = tan radius in
                 renderQuadric (QuadricStyle Nothing NoTextureCoordinates Outside FillStyle) (Sphere euclid_radius 10 10)
 
 update :: State -> State

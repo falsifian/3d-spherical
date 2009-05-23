8e41877a70c0dfc20fcdab966501ad621e9080c6
james <james@glider.falsifian.org>
2009-05-22 19:10:46 -0700

    Added a stairway.

--- Architecture.hs
+++ Architecture.hs
@@ -1,5 +1,6 @@
 module Architecture where
 
+import Constants
 import qualified Graphics.Rendering.OpenGL.GL as GL
 import List
 import Math
@@ -10,7 +11,20 @@ data Floating_Triangle = FTri { height :: Double
                               }
 
 world_arch :: [Floating_Triangle]
-world_arch = [FTri 0.115 (GL.Color3 1 0 1) (V3 1 0 0) (V3 0 1 0) (V3 0 0 1), FTri 0.117 (GL.Color3 0 1 1) (V3 0 1 0) (V3 1 0 0) (normalize (V3 0.5 0.5 (-0.5)))]
+world_arch = [FTri 0.115 (GL.Color3 1 0 1) (V3 1 0 0) (V3 0 1 0) (V3 0 0 1), FTri 0.117 (GL.Color3 0 1 1) (V3 0 1 0) (V3 1 0 0) (normalize (V3 0.5 0.5 (-0.5)))] ++ stairs
+
+stairs :: [Floating_Triangle]
+stairs = concat
+    [   let p0 = normalize (V3 (cos (t0 * r)) (sin (t0 * r)) (-zw / 2))
+            p1 = normalize (V3 (cos (t1 * r)) (sin (t1 * r)) (-zw / 2))
+            p2 = normalize (V3 (cos (t1 * r)) (sin (t1 * r)) (zw / 2))
+            p3 = normalize (V3 (cos (t0 * r)) (sin (t0 * r)) (zw / 2))
+        in
+        [FTri (t0 * pi) (GL.Color3 0.7 0.5 0.2) p0 p1 p2, FTri (t0 * pi) (GL.Color3 0.7 0.5 0.2) p0 p2 p3]
+    | t0 <- [0.1 / pi, 0.11 / pi .. 0.2 / pi], t1 <- [t0 + 0.01 / pi]
+    ]
+    where r = 100
+          zw = 0.2
 
 draw_ft :: Floating_Triangle -> IO ()
 draw_ft (FTri height color a b c) =
@@ -18,7 +32,7 @@ draw_ft (FTri height color a b c) =
        GL.renderPrimitive GL.TriangleStrip $ sequence_ $
            concat [GL.primitiveRestart : map GL.vertex (one_row i) | i <- [0 .. pred side_segments]]
     where
-        side_segments = 10 -- TODO: calculate this properly
+        side_segments = maximum [f a b, f b c, f c a] where f x y = round (1 * sin height * norm (a @- b) / player_height)
         one_row i = map (vec4_to_vertex4 . fix_height . interp) $
             (i, side_segments - i, 0) :
             concat [[(i + 1, side_segments - i - j, j - 1), (i, side_segments - i - j, j)] | j <- [1 .. side_segments - i]]
